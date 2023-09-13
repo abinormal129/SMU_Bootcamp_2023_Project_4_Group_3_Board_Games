1d050a90720beadd40ff5f4a9bf132552f6370f2
amperez65 <amperez65@hotmail.com>
2023-09-12 22:25:03 -0500

    website push

--- website/app.py
+++ website/app.py
@@ -43,6 +43,23 @@ def sample_data():
 def write_up():
     return render_template("write_up.html")
 
+
+
+#############################################################
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
 # run the website
-if __name__ == '__main__':
+#main
+if __name__ == "__main__":
     app.run(debug=True)
